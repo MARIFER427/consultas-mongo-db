3147abbc2a26d1572d7b7a32f24e8fabc06e2473
MARIFER427 <mariafernanda.demezabermudez.gen23@cbtis105.edu.mx>
2025-04-02 10:05:31 -0600

    dia2

--- Controllers/Api/EqController.cs
+++ Controllers/Api/EqController.cs
@@ -1,9 +1,71 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 [ApiController]
 [Route("api/eq")]
 public class EqController : Controller {
     [HttpGet("listar-agencias")]
+
     public IActionResult ListarAgencias(){
-        return Ok();
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.Agencia, "Inmobiliaria Pére");
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+
+     [HttpGet("listar-tipo")]
+    public IActionResult ListarTipo(){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.Tipo, "Terreno");
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+
+[HttpGet("listar-operacion")]
+     public IActionResult ListarOperacion(){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.Operacion, "Venta");
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+
+    [HttpGet("listar-banio")]
+     public IActionResult ListarBanios(int banios){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.Banios, banios);
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+
+    [HttpGet("listar-construccion")]
+     public IActionResult ListarMetrosConstruccion(int MetrosConstruccion){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.MetrosConstruccion, MetrosConstruccion);
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
     }
 }
---
+++ Controllers/Api/GtController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+[ApiController]
+[Route("api/gt")]
+public class GtController : Controller {
+ [HttpGet("listar-pisos")]
+     public IActionResult ListarPisos(int pisos){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.Pisos, pisos);
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+
+     [HttpGet("listar-banios")]
+     public IActionResult ListarBanios(int banios){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.Banios, banios);
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+    [HttpGet("listar-terreno")]
+     public IActionResult ListarMetrosTerrreno(int metrosterreno){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.MetrosTerreno, metrosterreno);
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+[HttpGet("listar-costo")]
+     public IActionResult ListarCosto(int costo){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.Costo, costo);
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+    [HttpGet("listar-fechapublicacion")]
+     public IActionResult ListarFechaPublicacion(string fechapublicacion){
+
+        MongoClient client = new  MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.FechaPublicacion, fechapublicacion);
+
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);
+    }
+}
